170e87bedee64f68a03660a26bcbf5a4726efbba
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2024-01-09 09:42:42 +0000

    Create pokeapi fetch in seed service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { CommonModule } from './common/common.module';
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest-pokemon'),
     PokemonModule,
     CommonModule,
+    SeedModule,
   ],
   controllers: [],
   providers: [],
